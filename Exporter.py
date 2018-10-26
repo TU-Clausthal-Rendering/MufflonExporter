ab574b58d28e38a290535ce2e6c7cd9ceb5fbe8a
Darkwilli <darkwilli@t-online.de>
2018-10-26 11:58:37 +0200

    finished camera export and corrected transition from right to left handed system (again)

--- Exporter.py
+++ Exporter.py
@@ -26,9 +26,31 @@ for i in range(len(cameras)):
     cameraObject = bpy.data.objects[camera.name]
     cameraType = ""
     if camera.type == "PERSP":
-        cameraType = "focus"
+        aperture = camera.gpu_dof.fstop
+        if aperture == 128.0:
+            cameraType = "pinhole"
+            dataDictionary['cameras'][camera.name] = collections.OrderedDict()
+            dataDictionary['cameras'][camera.name]['type'] = cameraType
+            fov = camera.angle * 180 / 3.141592653589793  # convert rad to degree
+        else:
+            cameraType = "focus"
+            dataDictionary['cameras'][camera.name] = collections.OrderedDict()
+            dataDictionary['cameras'][camera.name]['type'] = cameraType
+            focalLength = camera.length
+            dataDictionary['cameras'][camera.name]['focalLength'] = focalLength
+            focalDistance = camera.dof_distance
+            dataDictionary['cameras'][camera.name]['focalDistance'] = focalDistance
+            chipHeight = camera.sensor_height
+            dataDictionary['cameras'][camera.name]['chipHeight'] = chipHeight
+            dataDictionary['cameras'][camera.name]['aperture'] = aperture
     elif camera.type == "ORTHO":
         cameraType = "ortho"
+        dataDictionary['cameras'][camera.name] = collections.OrderedDict()
+        dataDictionary['cameras'][camera.name]['type'] = cameraType
+        orthoWidth = camera.ortho_scale
+        dataDictionary['cameras'][camera.name]['width'] = orthoWidth
+        orthoHeight = scn.render.resolution_y / scn.render.resolution_x * orthoWidth  # get aspect ratio via resolution
+        dataDictionary['cameras'][camera.name]['height'] = orthoHeight
     else:
         print("Skipping unsupported camera type: \"%s\" from: \"%s\"" % camera.type, camera.name)
         continue
@@ -43,15 +65,13 @@ for i in range(len(cameras)):
             x_pos = x_curve.evaluate(f)
             y_pos = y_curve.evaluate(f)
             z_pos = z_curve.evaluate(f)
-            cameraPath.append([x_pos, y_pos, z_pos])
-    else: # for not animated Cameras
-        cameraPath.append([cameraObject.location.x, cameraObject.location.y, cameraObject.location.z])
+            cameraPath.append([x_pos, z_pos , y_pos])
+    else:  # for not animated Cameras
+        cameraPath.append([cameraObject.location.x, cameraObject.location.z, cameraObject.location.y])
         viewDirection = cameraObject.matrix_world.to_quaternion() * Vector((0.0, .0, -1.0))
         viewDirectionPath.append([viewDirection.x, viewDirection.z, viewDirection.y])
         up = cameraObject.matrix_world.to_quaternion() * Vector((0.0, 1.0, 0.0))
         upPath.append([up.x, up.z, up.y])
-    dataDictionary['cameras'][camera.name] = collections.OrderedDict()
-    dataDictionary['cameras'][camera.name]['type'] = cameraType
     dataDictionary['cameras'][camera.name]['path'] = cameraPath
     dataDictionary['cameras'][camera.name]['viewDir'] = viewDirectionPath
     dataDictionary['cameras'][camera.name]['up'] = upPath
@@ -75,7 +95,7 @@ for i in range(len(lamps)):
         dataDictionary['lights'][lamp.name]['type'] = lightType
         viewDirection = lampObject.matrix_world.to_quaternion() * Vector((0.0, 0.0, -1.0))
         dataDictionary['lights'][lamp.name]['direction'] = [viewDirection.x, viewDirection.z, viewDirection.y]
-        dataDictionary['lights'][lamp.name]['irradiance'] = "PlaceHolder" # TODO
+        dataDictionary['lights'][lamp.name]['irradiance'] = "PlaceHolder"  # TODO
         dataDictionary['lights'][lamp.name]['scale'] = "PlaceHolder"  # TODO
     elif lamp.type == "SPOT":
         lightType = "spot"
